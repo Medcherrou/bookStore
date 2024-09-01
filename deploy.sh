3df7290ba5a08923e53cdaea8c19d0aa2aa446ba
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2024-09-02 00:54:41 +0100

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Use the correct Ruby version
+# Ensure Ruby is set to the correct version
 rvm use 3.0.0 --default
 ruby -v
 
-# Force push to Heroku, ignoring uncommitted changes
-git push heroku main --force
+# Stage and commit any modified or untracked files
+git add -A  # This stages all changes, including untracked files
+git commit -m "Auto-commit changes during deployment"
+
+# Push the changes to Heroku
+git push heroku HEAD:refs/heads/main --force  # Force push to the main branch
