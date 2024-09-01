850a453851e22390083bc74a99b1f157f85b2ece
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2024-09-01 15:31:25 +0100

    initial commit

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.dto.BookDto;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    ResponseEntity<List<BookDto>> getBooks(){
+        BookDto bookDto = BookDto.builder().title("First Book").build();
+        List<BookDto> bookDtos = new ArrayList<>();
+        bookDtos.add(bookDto);
+        return ResponseEntity.ok(bookDtos);
+    }
+}
