d793f02f73e0b98b767a9f02c8d81f0661caf183
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2024-09-02 00:43:27 +0100

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+# Ensure Ruby is set to the correct version
 rvm use 3.0.0 --default
 ruby -v
 
-# Heroku deployment commands here
+# Add any modified or untracked files
+git add -A
+
+# Commit the changes with a message
+git commit -m "Auto-commit during deployment"
+
+# Deploy to Heroku
 git push heroku main
