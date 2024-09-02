131d604d1c3c7c6761ed8eb4037afd8754d55b28
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2024-09-02 01:05:28 +0100

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+language: java
+jdk:
+  - openjdk17
 
-set -e  # Exit immediately if a command exits with a non-zero status
+before_install:
+  - chmod +x gradlew
+  - bash -c "source ~/.rvm/scripts/rvm"
+  - rvm install 3.0.0 --default
+  - rvm use 3.0.0 --default
+  - ruby -v
+  - gem install bundler
+  - bundle install
+  - chmod +x deploy.sh
 
-# Use the correct Ruby version
-rvm use 3.0.0 --default
-ruby -v
+script:
+  - ./gradlew assemble
 
-# Create a new temporary branch
-git checkout -b deploy-branch
-
-# Stage and commit any modified or untracked files
-git add -A  # Stage all changes
-git commit -m "Auto-commit changes during deployment"
-
-# Push the temporary branch to Heroku
-git push heroku deploy-branch:refs/heads/main --force
-
-# Clean up by deleting the temporary branch
-git checkout -
-git branch -D deploy-branch
+deploy:
+  provider: heroku
+  api_key: $HEROKU_TOKEN
+  app: "book-demo-store"
+  on:
+    branch: main
